a433c627c2ebd8763e07df9c89a7030f88b23097
Valerio Cietto <valerio_ciet@hotmail.it>
2015-08-07 19:39:36 +0200

    Create ModularServer.ino

---
+++ ModularServer.ino
@@ -0,0 +1,43 @@
+
+#include "ESP8266WiFi.h"
+void setup() {
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
+  Serial.println("scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"no encryption found");
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+}
+
+void loop() {
+  
+
+}
